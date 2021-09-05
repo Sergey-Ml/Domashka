02cb3e6bc21494ae64e7f3f11c2a56097829c945
Sergey_Ml <sergey@bukatelecom.ru>
2021-09-05 20:28:36 +0300

    финал

---
+++ ChessBoard_SP3/ChessBoard_SP3.cpp
@@ -0,0 +1,76 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int razmer, i, j, k, x;
+	cout << "Введите размер (четное число): \n";
+	cin >> razmer;
+	while (razmer % 2 != 0)
+	{
+		razmer = razmer + 1;
+	}
+	cout << "   "; //сдвиг верхней оконтовки
+	setlocale(LC_ALL, "C");
+	cout << char(201);
+	for (int y = 1; y <= razmer * razmer * 2; y++)
+		cout << char(205);
+	cout << char(187);
+	cout << "\n";
+	for (k = 0; k < razmer; k++)
+	{
+
+		for (i = 0; i < razmer; i++) //строки
+		{
+			cout << "   ";
+			cout << char(186);
+
+			for (j = 0; j < razmer; j++) //столбц
+
+			{
+
+				for (x = 0; x < razmer/2; x++)
+
+				{
+
+					if ((k + j) % 2 == 0)
+					{
+						
+						//for (j = 0; j <= razmer / 2; j++)
+						{
+							if (i % 2 != 0)
+							{
+								cout << "  ";
+								cout << char(219)<<char(219);
+							}
+							else {
+								
+								cout << char(219)<< char(219);
+								cout << "  ";
+							}
+						}
+						}
+					else
+					{
+						cout << "    ";
+
+					}
+
+
+				}
+
+			}
+			cout << char(186);
+			cout << " \n";
+		}
+
+	}
+	cout << "   ";
+	cout << char(200);
+	for (int y = 1; y <= razmer * razmer * 2; y++)
+	{
+		cout << char(205);
+	}
+	cout << char(188);
+	cout << "\n";
+	cout << "     ";
+}
