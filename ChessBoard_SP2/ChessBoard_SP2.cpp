e17f74d0b95bd2dfe24e0095b5c03f8e1370c9c0
Sergey_Ml <sergey@bukatelecom.ru>
2021-09-05 17:38:05 +0300

    final

---
+++ ChessBoard_SP2/ChessBoard_SP2.cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int razmer, i, j, k, x;
+	cout << "Введите размер: \n";
+	cin >> razmer;
+	cout << "   "; //сдвиг верхней оконтовки
+	setlocale(LC_ALL, "C");
+	cout << char(201);
+	for (int y = 1; y <= razmer*razmer*2; y++)
+			cout << char(205);
+	cout << char(187);
+	cout << "\n";
+	for (k = 0; k < razmer; k++)
+	{
+	
+		for (i = 0; i < razmer; i++) //строки
+		{
+			cout << "   ";
+			cout << char(186);
+			
+			for (j = 0; j < razmer; j++) //столбц
+
+			{
+			
+				for (x = 0; x < razmer; x++)
+				
+				{
+					
+					if ((k + j) % 2 == 0)
+					{
+						cout << char(219)<< char(219);
+					}
+					else
+					{
+						cout << "  ";
+						
+					}
+					
+					
+				}
+				
+			}
+			cout << char(186);
+			cout << " \n";
+			}
+		
+	}
+	cout << "   ";
+	cout << char(200);
+	for (int y = 1; y <= razmer * razmer * 2; y++)
+	{
+		cout << char(205);
+	}
+	cout << char(188);
+	cout << "\n";
+	cout << "     ";
+}
--- test/test.cpp
+++ test/test.cpp
@@ -1,25 +1,71 @@
 ﻿#include<iostream>
 using namespace std;
-int main()
-{
+int main() {
 	setlocale(LC_ALL, "Russian");
-	int razmer = 5, i, j;
-	//cout << "Введите размер доски :\n";
-//cin >> razmer;
-//cout << "\n";
-
-	for (i = 0; i < razmer; i++) //строк
+	int razmer = 5, i, j, k, x;
+	//cout << "Введите размер доски не более 30: \n";
+	//cin >> razmer;
+	cout << "\n\n";
+	//while (razmer > 30)
+	//{
+		//razmer = 30;
+	//}
+	while (razmer % 2 != 0)
 	{
-		for (j = 0; j < razmer; j++) //столб
-		{
-			for (int x = 0; x < razmer; x++) 
-			{
-				if ((i + j) % 2 == 0)
-					cout << " * ";
-				else
-					cout << "   ";
-			}
-		}
-		cout << "\n\n";
+		razmer = razmer + 1;
 	}
+	setlocale(LC_ALL, "C");
+	for (k = 1; k < razmer; k++) {
+		cout << "     "; //сдвиг верхней оконтовки
+		cout << char(201);
+		for (int y = 1; y <= razmer * 2; y++) {
+			cout << char(205);
+		}
+		cout << char(187);
+		cout << "\n";
+		for (i = 1; i <= razmer; i++)
+		{
+			cout << "     "; //сдвиг всей таблицы
+			cout << char(186);
+			for (j = 1; j <= razmer / 2; j++)
+			{
+				if (i % 2 != 0) {
+					cout << "  ";
+					cout << char(219) << char(219);
+				}
+				else {
+					cout << char(219) << char(219);
+					cout << "  ";
+				}
+				for (x = 0; x < razmer; x++)
+				{
+					if ((k + j) % 2 == 0)
+					{
+						cout << "  ";
+						cout << char(219) << char(219);
+					}
+					else
+					{
+						cout << char(219) << char(219);
+						cout << "   ";
+					}
+				}
+				cout << "\n\n";
+			}
+			cout << char(186);
+			cout << "  ";
+			cout << "\n";
+		}
+		cout << "     "; //сдвиг нижней оонтовки
+		cout << char(200);
+		for (int y = 1; y <= razmer * 2; y++)
+		{
+			cout << char(205);
+		}
+		cout << char(188);
+		cout << "\n";
+		cout << "     "; //сдвиг нижней строки букв
+		cout << "\n";
+	}
+	cout << "\n\n\n\n\n\n";
 }
